345666f13a99e9bb579b20744f671b857e2e2cbc
Duchstf <minhduc8199@gmail.com>
2024-01-21 11:32:58 -0600

    modify run script

--- Serenity_APx/APx_run/run_APx_board.sh
+++ Serenity_APx/APx_run/run_APx_board.sh
@@ -5,14 +5,14 @@ bitfile=/scratch/dhoang/SC_multiboard/Serenity_APx/bitfiles/SC_APd1.bit
 apx-prime -H localhost -c config.ini -s "device fpga.bitfile=$bitfile"
 
 #Loop through everything
-N=1
+N=0
 
 for i in $(seq 0 $N)
 do
     sh pattern_test.sh \
-     sparrow4-linux \ 
-     Rx_config.txt \ 
-     Tx_config.txt \ 
-     ../CL2_APx_inputs/l2_apx_SC_inputs_$i.txt \
-     ../CL2_APx_outputs/l2_apx_SC_output_$i.txt.txt 0 0
+    sparrow4-linux \
+    Rx_config.txt \
+    Tx_config.txt \
+    ../CL2_APx_inputs/l2_apx_SC_inputs_$i.txt \
+    ../CL2_APx_outputs/l2_apx_SC_output_$i.txt.txt 0 0
 done
