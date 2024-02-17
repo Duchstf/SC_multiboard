f77cda199fa156cdbc6f0372101b0d40c64a0b24
Duchstf <minhduc8199@gmail.com>
2024-02-17 15:37:04 -0600

    make CL2 inputs

--- APx_APx/CL2_APx_run/run_CL2_single_test.sh
+++ APx_APx/CL2_APx_run/run_CL2_single_test.sh
@@ -12,5 +12,5 @@ sh pattern_test.sh \
 sparrow5-linux \
 Rx_config.txt \
 Tx_config.txt \
-../CL1_APx_barrel_outputs/out_combined2.txt \
-../CL2_APx_outputs/l2_apx_SC_output_combined2.txt 0 0
+../CL1_APx_barrel_outputs/CL1_inputs_0.txt \
+../CL2_APx_outputs/CL2_out_0.txt 0 0
--- APx_APx/make_CL2_inputs.py
+++ APx_APx/make_CL2_inputs.py
@@ -12,44 +12,28 @@ def main():
     #Create data entries for each clock
     for clock in range(0,1024): #Each file holds maximum 1024 clocks
         apx_clock = f"0x{clock:04x}"
-        apx_dict[apx_clock] = ['0x0000000000000000'] * 100
+        apx_dict[apx_clock] = ['0x0000000000000000'] * 6
     
-    #Read the yaml config
-    with open("CL1_CL2_linkmap.yml", 'r') as stream:
-        link_map = yaml.safe_load(stream)['link_map']
-    
-    for i in range(0,84):
-        for board in link_map:
-            filename = '{}_{}.txt'.format(list(board.keys())[0],i)
-            links = list(board.values())[0]
-            
-            og_links = list(range(int(links.split(':')[0].split('-')[0]), int(links.split(':')[0].split('-')[1])+1))
-            remapped_links = list(range(int(links.split(':')[1].split('-')[0]), int(links.split(':')[1].split('-')[1])+1))
-            assert len(og_links) == len(remapped_links)
-            
-            print("Remapping {} to {} in {}.".format(og_links, remapped_links, filename))
-            
-            #Read the file in
-            with open("CL1_Serenity_outputs/{}".format(filename), 'r') as istr:
-                for j, line in enumerate(istr):
-                    
-                    if j > 3: #Skip the first 3 lines
-                        emp_clock = int(line[6:10])
-                        apx_clock = f"0x{emp_clock:04x}"
+    for i in range(0,1):
+        #Input file name
+        filename = 'CL1_out_{}.txt'.format(i)
+        
+        #Read the file in
+        with open("CL1_APx_barrel_outputs/{}".format(filename), 'r') as istr:
+            for j, line in enumerate(istr):
+                
+                if j > 1: #Skip the first 2 lines
+                    stripped_line =  list(line.rstrip('\n').split())
+                    if len(stripped_line) > 0:
+                        clock = stripped_line[0]
                         
                         #Convert link data
-                        emp_data = line[11:].rstrip('\n').split()
+                        data = stripped_line[1:]
                         
-                        #Filter out the sideband data (let APx sim handle this)
-                        apx_data = ['0x'+value for value in emp_data if len(value) > 4]
-                        
-                        #Remap
-                        for l in range(0,len(og_links)):
-                            apx_dict[apx_clock][remapped_links[l]] = apx_data[og_links[l]]
-        
+                        apx_dict[clock] = data
         
         #Print this all out in an APx formmatted file
-        APx_filename='CL2_APx_inputs/l2_apx_SC_inputs_{}.txt'.format(i)
+        APx_filename='CL2_APx_inputs/CL2_input_{}.txt'.format(i)
         
         with open(APx_filename, 'w') as ostr:
             
@@ -59,7 +43,7 @@ def main():
             #Create link lables
             print("#LinkLabel", file=ostr, end='')
             print("           LINK_00", file=ostr, end='')
-            label_list = ['               LINK_{:02d}'.format(x) for x in range(1,100)]
+            label_list = ['               LINK_{:02d}'.format(x) for x in range(1,6)]
             print(''.join(label_list), file=ostr)
             
             print("#BeginData", file=ostr)
