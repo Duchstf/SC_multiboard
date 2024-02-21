dd5e3a7b1b0e250ae468af4b68374e9daa4edacc
Duchstf <minhduc8199@gmail.com>
2024-02-21 11:00:47 -0600

    modify testing script

--- APx_APx/CL2_APx_run/run_CL2_all.sh
+++ APx_APx/CL2_APx_run/run_CL2_all.sh
@@ -1,11 +1,11 @@
 #Set the bitfile path
-bitfile=/scratch/dhoang/SC_multiboard/Serenity_APx/bitfiles/SC_APd1_v2.bit
+bitfile=/scratch/dhoang/SC_multiboard/APx_APx/bitfiles/SC_APd1_v2.bit
 
 #Program the FPGA
 apx-prime -H localhost -c config.ini -s "device fpga.bitfile=$bitfile"
 
 #Loop through everything
-N=83
+N=9
 
 #Make output directory
 mkdir -p ../CL2_APx_outputs
@@ -17,6 +17,6 @@ do
     sparrow5-linux \
     Rx_config.txt \
     Tx_config.txt \
-    ../CL2_APx_inputs/l2_apx_SC_inputs_$i.txt \
-    ../CL2_APx_outputs/l2_apx_SC_output_$i.txt 0 0
+    ../CL2_APx_inputs/CL2_input_$i.txt \
+    ../CL2_APx_outputs/CL2_input_$i.txt 0 0
 done
